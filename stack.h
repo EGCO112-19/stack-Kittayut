ec92f77a6bdc1874ac7d0c9984f8dd059f2af099
Kittayut <kittayut.m@gmail.com>
2021-03-04 17:33:25 +0000

    Homework Edited MK.2

--- stack.h
+++ stack.h
@@ -13,14 +13,14 @@ char pop(StackPtr s);
 void pop_all(StackPtr s);
 
 void push(StackPtr s, char value){
-  //printf("%c\n",value);
+  
   Node *new_node=(Node *)malloc(sizeof(Node));
   if(new_node){
     new_node-> data = value;
     new_node-> nextPtr = s->top;
     s->top = new_node;
     s->size += 1;
-    //printf("%d ..\n",s->size);
+    
   }
 }
 // s size top
