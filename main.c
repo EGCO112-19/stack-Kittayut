21914ee5d2ba249ec61e062b2147e6ac4eadd1c6
Kittayut <kittayut.m@gmail.com>
2021-03-04 17:29:48 +0000

    Homework edited

--- main.c
+++ main.c
@@ -8,22 +8,53 @@ int main(int argc, char **argv){
   int i,N,j;
   
   Stack s;
-
-
- 
+  s.size = 0;
+  s.top = NULL;
+  char popvalue;
+  int check=0;
+  
+  
   for(i=1;i<argc;i++){
-   
      for(j=0;j<strlen(argv[i]);j++){
        /* Use stack to help with the parentheses*/
+       if(argv[i][j] == '[' || argv[i][j] == '{'){
+          push(&s, argv[i][j]); 
+       }
+       else if (argv[i][j] == ']'){
+          if (s.size==0){
+            check = 1;
+            break;
+          }
+          popvalue = pop(&s);
+          if (popvalue != '['){
+            check = 1;
+            break;
+          }
+       }
+       else if (argv[i][j] == '}'){
+         if (s.size==0){
+           check = 1;
+            break;
+          }
+          popvalue = pop(&s);
+          if (popvalue != '{'){
+            check = 1;
+            break;
+          }
+       }
 
+      
 
      }
-
+     if(s.size>0 || check != 0){
+         
+         printf("The parenthses do not match for %s \n", argv[i]);
+         pop_all(&s);
+       }else{
+         
+         printf("The parenthses match successfully for %s \n", argv[i]);
+       }
 
   }
-
-
-
-
    return 0;
 }
--- stack.h
+++ stack.h
@@ -11,4 +11,41 @@ typedef Stack * StackPtr;
 void push(StackPtr s, char value);
 char pop(StackPtr s);
 void pop_all(StackPtr s);
+
+void push(StackPtr s, char value){
+  //printf("%c\n",value);
+  Node *new_node=(Node *)malloc(sizeof(Node));
+  if(new_node){
+    new_node-> data = value;
+    new_node-> nextPtr = s->top;
+    s->top = new_node;
+    s->size += 1;
+    //printf("%d ..\n",s->size);
+  }
+}
+// s size top
+char pop(StackPtr s){
+  NodePtr t;
+  char value;
+  t = s->top;
+  value = t->data;
+  s->top = t->nextPtr;
+  free(t);
+  s->size -= 1;
+  return value;
+}
+
+void pop_all(StackPtr s){
+  NodePtr t = s->top;
+  char value;
+  while(s->top){
+    value = t->data;
+    s->top = t->nextPtr;
+    printf("Popping %c \n",value);
+    free(t);
+    t = s->top;
+    s->size = 0;
+  }
+}
+
 #endif
